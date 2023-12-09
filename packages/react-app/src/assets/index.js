a0bb3b1288f65c2cbea2657618ecda3fa42c279e
jscoding10 <jscoding10@protonmail.com>
2023-12-09 15:02:30 -0800

    Removed unnecessary assets

--- packages/react-app/src/assets/index.js
+++ packages/react-app/src/assets/index.js
@@ -1,7 +1,5 @@
 import chevronDown from "./chevron-down.svg";
-import ethereumLogo from "./ethereumLogo.png";
-import uniswapLogo from "./uniswapLogo.png";
 import vertexLogo from "./vertex-top-logo.png";
 import vertexLoading from "./vertex-logo-loading.png";
 
-export { chevronDown, ethereumLogo, uniswapLogo, vertexLogo, vertexLoading };
+export { chevronDown, vertexLogo, vertexLoading };
